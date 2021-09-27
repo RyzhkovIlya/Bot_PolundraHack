493d26f469b87e71cdcc1643a31162e0bbc1c852
Илья Рыжков <ryzhkov.ilya17@gmail.com>
2021-09-27 18:10:44 +0300

    fix

--- api/model.py
+++ api/model.py
@@ -1,29 +1,26 @@
 import os
-
-from dotenv import load_dotenv
 from google_images_search import GoogleImagesSearch
-# load_dotenv()
+from dotenv import load_dotenv
+
+
 def picture(text: str) -> str:
-    import os
-
-    from dotenv import load_dotenv
-    from google_images_search import GoogleImagesSearch
     load_dotenv()
-
-    gis = GoogleImagesSearch(os.getenv('TOKEN_GOOGLE'), os.getenv('GOOGLE_CX'))
-    '''
-    Возведение в куб
-    :param num:
-    :return:
-    '''
+    GCS_DEVELOPER_KEY = os.getenv('TOKEN_GOOGLE')
+    GCS_CX = os.getenv('GOOGLE_CX')
+    gis = GoogleImagesSearch(GCS_DEVELOPER_KEY, GCS_CX)
     _search_params = {
         'q': text,
         'num': 1,
-        'fileType': 'jpg'
-    }
-
+#        'safe': 'high',
+        'fileType': 'jpg'}
+#        'imgType': 'clipart|face|lineart|news|photo',
+#        'imgSize': 'huge|icon|large|medium|small|xlarge|xxlarge',
+#        'imgDominantColor': 'black|blue|brown|gray|green|orange|pink|purple|red|teal|white|yellow',
+#        'imgColorType': 'color|gray|mono|trans',
+#        'rights': 'cc_publicdomain|cc_attribute|cc_sharealike|cc_noncommercial|cc_nonderived'}
     gis.search(search_params=_search_params)
     for image in gis.results():
+        # take raw image data
         raw_image_data = image.get_raw_data()
         return raw_image_data
 # import os
